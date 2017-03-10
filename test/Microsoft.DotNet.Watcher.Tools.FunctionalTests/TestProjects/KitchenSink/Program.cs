5f38635f8b75874ab6939564b48dcddba7a85f8a
Nate McMaster <nate.mcmaster@microsoft.com>
2017-03-10 09:56:26 -0800

    Make it possible to detect that a process is running inside dotnet-watch by setting 'DOTNET_WATCH=1'

---
+++ test/Microsoft.DotNet.Watcher.Tools.FunctionalTests/TestProjects/KitchenSink/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace KitchenSink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Started");
+            Console.WriteLine("PID = " + Process.GetCurrentProcess().Id);
+            Console.WriteLine("DOTNET_WATCH = " + Environment.GetEnvironmentVariable("DOTNET_WATCH"));
+        }
+    }
+}
